f94eb523456d4d8271ef5ecb3a52fbf32f7ccf64
Leandro Ramos <leandroramos@disroot.org>
2022-05-03 20:22:59 -0300

    Fix redis_envvars.sh

--- redis_envvars.sh
+++ redis_envvars.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
+
 dokku redis:info nextcloud > /tmp/redis_info
+
 redis_host="$(grep -o -P '(?<=@).*(?=:)' /tmp/redis_info)"
 redis_port="$(grep -o -P '(?<=:)(\d+)(?!.*\d)(?=$)' /tmp/redis_info)"
 redis_host_password="$(grep -o -P '(?<=\/\/:).*(?=@)' /tmp/redis_info)"
 
-echo "redis_host: $redis_host"
-echo "redis_port: $redis_port"
-echo "redis_host_password: $redis_host_password"
+dokku config:set nextcloud REDIS_HOST=$redis_host REDIS_HOST_PASSWORD=$redis_host_password REDIS_PORT=$redis_port
