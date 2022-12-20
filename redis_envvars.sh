83af58100b3903ff8ac5deaec26a2e94308bed27
Leandro Ramos <leandroramos@disroot.org>
2022-12-20 15:19:28 -0300

    Remove harcoded appname in redis_envvars.sh
    Fixes #6
    
    - Now appname is a parameter sent by the ansible task.

--- redis_envvars.sh
+++ redis_envvars.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env sh
 
-dokku redis:info redis_alfa > /tmp/redis_info
+app_name=$1
+
+dokku redis:info redis_${app_name} > /tmp/redis_info
 
 redis_host="$(grep -o -P '(?<=@).*(?=:)' /tmp/redis_info)"
 redis_port="$(grep -o -P '(?<=:)(\d+)(?!.*\d)(?=$)' /tmp/redis_info)"
 redis_host_password="$(grep -o -P '(?<=\/\/:).*(?=@)' /tmp/redis_info)"
 
-dokku config:set alfa REDIS_HOST=$redis_host REDIS_HOST_PASSWORD=$redis_host_password REDIS_PORT=$redis_port
+dokku config:set ${app_name} REDIS_HOST=$redis_host REDIS_HOST_PASSWORD=$redis_host_password REDIS_PORT=$redis_port
