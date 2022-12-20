61ea004d705d255d9cc711e727e1852f9d5e23fe
Leandro Ramos <leandroramos@disroot.org>
2022-12-20 15:10:12 -0300

    Prepare the HTTPS deployment

--- redis_envvars.sh
+++ redis_envvars.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
-dokku redis:info redis_nextcloud > /tmp/redis_info
+dokku redis:info redis_alfa > /tmp/redis_info
 
 redis_host="$(grep -o -P '(?<=@).*(?=:)' /tmp/redis_info)"
 redis_port="$(grep -o -P '(?<=:)(\d+)(?!.*\d)(?=$)' /tmp/redis_info)"
 redis_host_password="$(grep -o -P '(?<=\/\/:).*(?=@)' /tmp/redis_info)"
 
-dokku config:set nextcloud REDIS_HOST=$redis_host REDIS_HOST_PASSWORD=$redis_host_password REDIS_PORT=$redis_port
+dokku config:set alfa REDIS_HOST=$redis_host REDIS_HOST_PASSWORD=$redis_host_password REDIS_PORT=$redis_port
